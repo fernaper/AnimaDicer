d6133a5cd6a90ce243b55024b2a737e400ba1148
Fernando Pérez <fernaper@ucm.es>
2017-12-08 17:49:26 +0100

    v1.0.6 - RC
    - New stable version
    - Updated the jar file and the last version
    
    Signed-off-by: Fernando Pérez <fernaperg@gmail.com>

--- src/animadicer/AnimaDicer.java
+++ src/animadicer/AnimaDicer.java
@@ -15,7 +15,7 @@ import java.io.InputStream;
  * @author Fernando
  */
 public class AnimaDicer {
-    private static final String VERSION = "v1.0.5";
+    private static final String VERSION = "v1.0.6";
     /**
      * @param args the command line arguments
      */
